e096cbd69b853a9d2f7e025f87f191833cc2637c
Luna Kilborn <leland.kilborn@hotmail.com>
2022-10-19 17:23:23 -0400

    Add progress bars

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,3 +1,8 @@
+use indicatif::ProgressBar;
+use indicatif::{self, ProgressStyle};
+
+const SUMPROGSTYLE: &str = "[{msg}] {eta} {wide_bar}";
+
 fn modpow(b: i32, e: i32, m: i32) -> i32 {
     let mut y: u128 = 1;
     let mut b: u128 = b as u128;
@@ -13,12 +18,17 @@ fn modpow(b: i32, e: i32, m: i32) -> i32 {
     return y as i32;
 }
 
-fn summation(n: i32, a: i32) -> f64 {
+fn summation(n: i32, a: i32, num: u8) -> f64 {
     let mut left = 0.0;
+    let style = ProgressStyle::with_template(SUMPROGSTYLE).unwrap();
+    let bar = ProgressBar::new(n as u64).with_style(style);
+    bar.set_message(format!("{} / {}", num, 4));
     for k in 0..=n {
         let r = 8 * k + a;
         left = (left + modpow(16, n - k, r) as f64 / r as f64).fract();
+        bar.inc(1);
     }
+    bar.finish();
 
     // let mut right = 0.0;
     // for k in (n + 1).. {
@@ -33,9 +43,9 @@ fn summation(n: i32, a: i32) -> f64 {
 }
 
 pub fn digit(n: i32) -> u8 {
-    let mut sum = 4.0 * summation(n, 1);
-    sum -= 2.0 * summation(n, 4);
-    sum -= summation(n, 5);
-    sum -= summation(n, 6);
+    let mut sum = 4.0 * summation(n, 1, 1);
+    sum -= 2.0 * summation(n, 4, 2);
+    sum -= summation(n, 5, 3);
+    sum -= summation(n, 6, 4);
     return (sum.rem_euclid(1.0) * 16.0) as u8;
 }
